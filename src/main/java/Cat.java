cea199037d9735513d494667523ecacbdc8acdba
Evgeny <vasilkalin@gmail.com>
2021-06-17 14:39:22 +0200

    Work variant

---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+public class Cat {
+    private String message;
+
+//    public Cat(String message) {
+//        this.message = message;
+//    }
+//
+//    public Cat() {
+//
+//    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
